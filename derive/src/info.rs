955c5c93bd489a29af1ad6099232a17710816308
thomas <thomas@dooms.eu>
2022-04-22 15:53:10 +0200

    composed pk

--- derive/src/info.rs
+++ derive/src/info.rs
@@ -1,6 +1,26 @@
 use quote::{quote, ToTokens};
 use syn::{Ident, Type};
 
+fn ty_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let inner_ty = inner_ty.args.first().unwrap();
+            if let syn::GenericArgument::Type(ref t) = inner_ty.value() {
+                return Some(t);
+            }
+        }
+    }
+    None
+}
+
 pub struct StructInfo {
     pub ident: Ident,
     pub name: String,
@@ -17,19 +37,19 @@ impl ToTokens for StructInfo {
             fields,
         } = self;
 
+        let encode_map = |(ident, _)| quote!(api::Encode::encode(&self.#ident));
+        let fmt_map = |(ident, ty)| match ty_inner_type("Option", ty) {
+            quote!(#ident: {})
+        }
+
         let pk_name = Ident::new(&format!("{}Pk", ident), ident.span());
         let pk_fields = pks.iter().map(|(ident, ty)| quote!(#ident: #ty));
 
         let idents = fields.iter().map(|(ident, _)| quote!(#ident: {}));
         let pk_idents = pks.iter().map(|(ident, _)| quote!(#ident: {}));
 
-        let params = fields
-            .iter()
-            .map(|(ident, _)| quote!(api::Encode::encode(&self.#ident)));
-
-        let pk_params = pks
-            .iter()
-            .map(|(ident, _)| quote!(api::Encode::encode(&self.#ident)));
+        let params = fields.iter().map(encode_map);
+        let pk_params = pks.iter().map(encode_map);
 
         let field_elems = fields.iter().map(|(ident, _)| quote!(Self::#ident()));
 
