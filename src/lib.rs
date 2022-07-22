6caf83205680454512d19d8dfdea72c4b2c2eee6
thomas <thomas@dooms.eu>
2022-07-22 22:04:18 +0200

    whoops

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub use crate::common::{Field, Fields, Object, Pk};
 pub use crate::conditions::*;
 pub use crate::delete::{DeleteBuilder, DeleteByPk, DeleteByPkBuilder};
 pub use crate::error::{Error, Result};
-pub use crate::fetch::{Fetch, Mutation, Mutation1, Mutation2, Query1, Query2, Queryable};
+pub use crate::fetch::{Fetch, Mutation, Mutation1, Mutation2, Mutation3, Query1, Query2, Query3, Queryable};
 pub use crate::insert::{Insert, InsertBuilder, InsertOne, InsertOneBuilder};
 pub use crate::macros::*;
 pub use crate::query::{Query, QueryBuilder, QueryByPk, QueryByPkBuilder};
