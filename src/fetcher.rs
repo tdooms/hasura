ef65fb486f420959d95ef302807f8f4c96c45114
thomas <thomas@dooms.eu>
2022-10-19 22:24:03 +0200

    add custom header to request

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use crate::{Error, Result};
 use serde_json::Value;
 use std::fmt::{Debug};
+use std::str::FromStr;
+use reqwest::header::{HeaderName, HeaderValue};
 
 #[derive(serde::Deserialize, Debug)]
 pub struct GraphqlError {
@@ -15,20 +18,15 @@ enum Response {
     Errors { errors: Vec<GraphqlError> },
 }
 
-async fn request(url: &str, body: String, t: Option<String>, a: Option<String>) -> Result<Value> {
-    let mut headers = reqwest::header::HeaderMap::new();
-
-    if let Some(token) = t {
-        headers.insert("authorization", token.parse().unwrap());
-    }
-
-    if let Some(admin) = a {
-        headers.insert("x-hasura-admin-secret", admin.parse().unwrap());
+async fn request(url: &str, body: String, headers: HashMap<String, String>) -> Result<Value> {
+    let mut temp = reqwest::header::HeaderMap::new();
+    for (key, value) in headers {
+        temp.insert(HeaderName::from_str(&key).unwrap(), HeaderValue::from_str(&value).unwrap());
     }
 
     let text = reqwest::Client::new()
         .post(url)
-        .headers(headers)
+        .headers(temp)
         .body(body)
         .send()
         .await?
@@ -44,34 +42,34 @@ async fn request(url: &str, body: String, t: Option<String>, a: Option<String>) 
 pub struct Fetcher<O> {
     pub body: String,
     pub extract: Box<dyn FnOnce(Value) -> Result<O>>,
-
-    pub token: Option<String>,
-    pub admin: Option<String>,
+    pub headers: HashMap<String, String>,
 }
 
 impl<O> Fetcher<O> {
     pub fn new<Fn: FnOnce(Value) -> Result<O> + 'static>(body: String, extract: Fn) -> Self {
-        let body = body.to_string().replace('"', "\\\"");
-        Self {
-            body: format!("{{\"query\": \"{}\"}}", body),
-            extract: Box::new(extract),
-            token: None,
-            admin: None
-        }
+        let inner = body.to_string().replace('"', "\\\"");
+        let body = format!("{{\"query\":\"{inner}\"}}");
+
+        Self { body, extract: Box::new(extract), headers: HashMap::new(), }
     }
 
-    pub fn token(mut self, token: impl Into<Option<String>>) -> Self {
-        self.token = token.into();
+    pub fn header(mut self, key: impl ToString, value: impl ToString) -> Self {
+        self.headers.insert(key.to_string(), value.to_string());
         self
     }
 
-    pub fn admin(mut self, admin: impl Into<Option<String>>) -> Self {
-        self.admin = admin.into();
+    pub fn admin(mut self, admin: impl ToString) -> Self {
+        self.headers.insert("x-hasura-admin-secret".to_string(), admin.to_string());
+        self
+    }
+
+    pub fn token(mut self, token: impl ToString) -> Self {
+        self.headers.insert("authorization".to_string(), token.to_string());
         self
     }
 
     pub async fn send(self, url: &str) -> Result<O> {
-        let val = request(url, self.body, self.token, self.admin).await?;
+        let val = request(url, self.body, self.headers).await?;
         (self.extract)(val)
     }
 }
